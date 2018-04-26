ac443cce7c57c0f2f0ab0e533b091fffe31b8edf
sarbada jaiswal <sarbadajaiswal@sarbadas-Air.lan>
2018-04-26 20:29:09 +0545

    modified container file

---
+++ src/components/IdeasContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class IdeasContainer extends Component {
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/ideas.json')
+    .then(response => {
+      console.log(response)
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
+
+  render() {
+    return (
+      <div>
+        Ideas
+      </div>
+    )
+  }
+}
+
+export default IdeasContainer
