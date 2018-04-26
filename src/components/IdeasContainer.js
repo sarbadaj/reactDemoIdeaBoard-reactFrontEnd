6278309ca2ecdbc56231ae03435a5d13266eada5
sarbada jaiswal <sarbadajaiswal@sarbadas-Air.lan>
2018-04-26 20:50:29 +0545

    added constructor for ideas mapping to the fetched value of ideas

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -3,6 +3,13 @@ import axios from 'axios'
 
 class IdeasContainer extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      ideas: []
+    }
+  }
+
   componentDidMount() {
     axios.get('http://localhost:3001/api/v1/ideas.json')
     .then(response => {
@@ -16,9 +23,16 @@ class IdeasContainer extends Component {
   render() {
     return (
       <div>
-        Ideas
+        {this.state.ideas.map((idea) => {
+          return(
+            <div className="tile" key={idea.id} >
+              <h4>{idea.title}</h4>
+              <p>{idea.body}</p>
+            </div>
+          )       
+        })}
       </div>
-    )
+    );
   }
 }
 
