15fbffac0cf086dfafdd6bd3372468cef2a9fbba
sarbada jaiswal <sarbadajaiswal@sarbadas-MacBook-Air.local>
2018-04-27 12:23:06 +0545

    added function for notification

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -29,11 +29,27 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  updateIdea = (idea) => {
+    const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+    const ideas = update(this.state.ideas, {
+      [ideaIndex]: { $set: idea }
+    })
+  }
+
+  resetNotification = () => {
+    this.setState({ notification: '' })
+  }
+
+  enableEditing = (id) => {
+    this.setState({editingIdeaId: id})
+  }
+
   constructor(props) {
     super(props)
     this.state = {
       ideas: [],
-      editingIdeaId: null
+      editingIdeaId: null,
+      notification: ''
     }
   }
 
@@ -41,7 +57,9 @@ class IdeasContainer extends Component {
     axios.get('http://localhost:3001/api/v1/ideas.json')
     .then(response => {
       console.log(response)
-      this.setState({ideas: response.data})
+      this.setState({ideas: response.data,
+                     notification: 'All changes have been saved.'})
+
     })
     .catch(error => console.log(error))
   }
@@ -53,11 +71,15 @@ class IdeasContainer extends Component {
         <button className="newIdeaButton" onClick={this.addNewIdea} >
           New Idea
         </button>
+        <span className="notification">
+          {this.state.notification}
+        </span>
         {this.state.ideas.map((idea) => {
           if(this.state.editingIdeaId === idea.id) {
-            return(<IdeaForm idea={idea} key={idea.id} />)
+            return(<IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea} resetNotification={this.resetNotification} 
+                    titleRef= {input => this.title = input}/>)
           } else {
-            return (<Idea idea={idea} key={idea.id} />)
+            return (<Idea idea={idea} key={idea.id} onClick={this.enableEditing} />)
           }
         })}
       </div>
